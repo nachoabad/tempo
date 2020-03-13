985531001c6ea1a70f3a4b16ad3d0ebccb83fa23
Nacho Abad <liao2512@gmail.com>
2020-03-13 09:49:46 +0100

    adds slot index and es locale

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -28,15 +28,15 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     post user_session_path,
       params: { user: { email: users(:one).email, password: 'useruser' } }
     follow_redirect!
-    assert_select 'h1', Date.today.to_s
-    assert_select 'a', 'Next'
-    assert_select "a", {count: 0, text: 'Back'}
+    assert_select 'p.name', Date.today.to_s
+    assert_select 'a', "Siguientes >>"
+    assert_select "a", {count: 0, text: "<< Anteriores"}
     assert_select 'a', '8:0'
 
     get service_slots_path(services(:one), date: Date.tomorrow.to_s)
-    assert_select 'h1', Date.tomorrow.to_s
-    assert_select 'a', 'Next'
-    assert_select "a", 'Back'
+    assert_select 'p.name', Date.tomorrow.to_s
+    assert_select 'a', "Siguientes >>"
+    assert_select "a", "<< Anteriores"
     assert_select 'a', '8:0'
 
     get new_slot_event_path(slots(:tomorrow_8am), date: Date.tomorrow.to_s)
@@ -52,16 +52,16 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
     assert_select 'td', Date.tomorrow.to_s
 
     get service_slots_path(services(:one), date: Date.tomorrow.to_s)
-    assert_select 'h1', (Date.today + 7).to_s
+    assert_select 'p.name', (Date.today + 7).to_s
 
     delete event_path(slots(:tomorrow_8am).events.on_date(Date.tomorrow).first)
     follow_redirect!
     assert_select 'div.alert', 'Event was successfully destroyed.'
     
     get service_slots_path(services(:one), date: Date.tomorrow.to_s)
-    assert_select 'h1', Date.tomorrow.to_s
-    assert_select 'a', 'Next'
-    assert_select "a", 'Back'
+    assert_select 'p.name', Date.tomorrow.to_s
+    assert_select 'a', "Siguientes >>"
+    assert_select "a", "<< Anteriores"
     assert_select 'a', '8:0'
   end
 end
