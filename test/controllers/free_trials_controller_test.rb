0f030616e1e0aef95a8b2fee1a6e6714f3f0a793
Nacho Abad <liao2512@gmail.com>
2020-03-21 19:05:14 +0100

    updates free trial controller test

--- test/controllers/free_trials_controller_test.rb
+++ test/controllers/free_trials_controller_test.rb
@@ -5,11 +5,6 @@ class FreeTrialsControllerTest < ActionDispatch::IntegrationTest
     @free_trial = free_trials(:one)
   end
 
-  test "should get index" do
-    get free_trials_url
-    assert_response :success
-  end
-
   test "should get new" do
     get new_free_trial_url
     assert_response :success
@@ -20,29 +15,6 @@ class FreeTrialsControllerTest < ActionDispatch::IntegrationTest
       post free_trials_url, params: { free_trial: { contact_time: @free_trial.contact_time, email: @free_trial.email, name: @free_trial.name, phone: @free_trial.phone } }
     end
 
-    assert_redirected_to free_trial_url(FreeTrial.last)
-  end
-
-  test "should show free_trial" do
-    get free_trial_url(@free_trial)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_free_trial_url(@free_trial)
-    assert_response :success
-  end
-
-  test "should update free_trial" do
-    patch free_trial_url(@free_trial), params: { free_trial: { contact_time: @free_trial.contact_time, email: @free_trial.email, name: @free_trial.name, phone: @free_trial.phone } }
-    assert_redirected_to free_trial_url(@free_trial)
-  end
-
-  test "should destroy free_trial" do
-    assert_difference('FreeTrial.count', -1) do
-      delete free_trial_url(@free_trial)
-    end
-
-    assert_redirected_to free_trials_url
+    assert_redirected_to root_path
   end
 end
