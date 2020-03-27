0cc5057f9a85c27ae1c37aa84f3cd38c2406182e
Nacho Abad <liao2512@gmail.com>
2020-03-27 21:26:28 +0100

    adds block and unblock date on slot blocker test

--- test/models/slot_blocker_test.rb
+++ test/models/slot_blocker_test.rb
@@ -24,4 +24,44 @@ class SlotBlockerTest < ActiveSupport::TestCase
     end
     assert Event.unscoped.last.blocked?
   end
+
+  test 'unblock a reserved event' do
+    slot = slots(:week_start_reserved).id
+    date = Date.today.beginning_of_week.to_s
+
+    assert_not slots(:week_start_reserved).available_on_date?(date)
+    assert_difference('Event.count', -1) do
+      SlotBlocker.new(blocker_params: {slot: slot, date: date}, service: @service).unblock!
+    end
+    assert slots(:week_start_reserved).available_on_date?(date)
+  end
+
+  test 'unblock a block event' do
+    slot = slots(:week_start_blocked).id
+    date = Date.today.beginning_of_week.to_s
+
+    assert_not slots(:week_start_blocked).available_on_date?(date)
+    assert_difference('Event.count', -1) do
+      SlotBlocker.new(blocker_params: {slot: slot, date: date}, service: @service).unblock!
+    end
+    assert slots(:week_start_blocked).available_on_date?(date)
+  end
+
+  test 'block a date' do
+    date = Date.today.beginning_of_week
+
+    assert_difference('Event.count', 1) do
+      SlotBlocker.new(blocker_params: {date: date.to_s}, service: @service).block!
+    end
+    assert @service.slots.available_on_date(date).empty?
+  end
+
+  test 'unblock a date' do
+    date = Date.today.beginning_of_week
+
+    assert_difference('Event.count', -3) do
+      SlotBlocker.new(blocker_params: {date: date.to_s}, service: @service).unblock!
+    end
+    assert_equal @service.slots.available_on_date(date).count, 3
+  end
 end
